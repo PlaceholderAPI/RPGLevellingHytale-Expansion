983159cc319d816f18836492e70a7af9cb501b3b
PiggyPiglet <noreply@piggypiglet.me>
2026-03-19 18:48:06 +0800

    rpglevelling fix 1.0.1 release

--- src/main/java/at/helpch/papi/expansion/rpglevelling/RPGLevellingExpansion.java
+++ src/main/java/at/helpch/papi/expansion/rpglevelling/RPGLevellingExpansion.java
@@ -2,7 +2,9 @@ package at.helpch.papi.expansion.rpglevelling;
 
 import at.helpch.placeholderapi.PlaceholderAPI;
 import at.helpch.placeholderapi.expansion.PlaceholderExpansion;
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.zuxaw.plugin.api.RPGLevelingAPI;
@@ -22,7 +24,7 @@ public class RPGLevellingExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.0.0";
+        return "1.0.1";
     }
 
     @Override
@@ -43,26 +45,21 @@ public class RPGLevellingExpansion extends PlaceholderExpansion {
 
     @Override
     public @Nullable String onPlaceholderRequest(final PlayerRef player, @NotNull final String params) {
-        switch (params) {
-            case "xp":
-                return String.valueOf(api.getPlayerXP(player.getUuid()));
-            case "level":
-                return String.valueOf(api.getPlayerLevel(player.getUuid()));
-            case "is_max":
-                return PlaceholderAPI.booleanValue(api.isPlayerMaxLevel(player.getUuid()));
+        final Ref<EntityStore> ref = player.getReference();
+
+        if (ref == null) {
+            return null;
         }
 
-        final RPGLevelingAPI.PlayerLevelInfo info = api.getPlayerLevelInfo(player.getUuid());
+        final RPGLevelingAPI.PlayerLevelInfo info = api.getPlayerLevelInfo(player, ref.getStore());
 
-        if (info != null) {
-            switch (params) {
-                case "maxlevel":
-                    return String.valueOf(info.getMaxLevel());
-                case "xpnextlevel":
-                    return String.valueOf(info.getXpNeededForNext());
-            }
-        }
-
-        return null;
+        return switch (params) {
+            case "xp" -> String.valueOf(info.getExperience());
+            case "level" -> String.valueOf(info.getLevel());
+            case "is_max" -> PlaceholderAPI.booleanValue(info.isMaxLevel());
+            case "maxlevel" -> String.valueOf(info.getMaxLevel());
+            case "xpnextlevel" -> String.valueOf(info.getXpNeededForNext());
+            default -> null;
+        };
     }
 }
